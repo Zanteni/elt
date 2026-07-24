561a7470eeaa9a64e502a4a6330d174405587057
Wajdi Zanteni <wajdizanteni2003@gmail.com>
2026-07-24 21:55:15 +0100

    Implment evaluation metrics

--- src/eval.py
+++ src/eval.py
@@ -1,11 +1,119 @@
-# # every K steps compute FID (val images and generated images)
+import torch
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
 
-# FID
 
-# Reconstruction Metrics
+@torch.no_grad()
+def evaluate_reconstruction(
+    model,
+    dataloader,
+    device="cuda",
+    num_images=8,
+):
+    """
+    Evaluate VAE reconstruction quality.
 
-# LPIPS
+    Returns:
+        reconstruction mse
+        original images
+        reconstructed images
+    """
 
-# (Optional)
+    model.eval()
+    model.to(device)
 
-# FDR
+
+    total_loss = 0
+    count = 0
+
+    originals = []
+    reconstructions = []
+
+
+    for images in dataloader:
+
+        images = images.to(device)
+
+
+        out = model(images)
+
+        recon = out["recon"]
+
+
+        loss = torch.mean(
+            (images - recon) ** 2
+        )
+
+
+        total_loss += loss.item()
+        count += 1
+
+
+        if len(originals) < num_images:
+
+            originals.append(
+                images.cpu()
+            )
+
+            reconstructions.append(
+                recon.cpu()
+            )
+
+
+        if len(originals) >= num_images:
+            break
+
+
+
+    originals = torch.cat(
+        originals,
+        dim=0
+    )[:num_images]
+
+
+    reconstructions = torch.cat(
+        reconstructions,
+        dim=0
+    )[:num_images]
+
+
+    return {
+        "reconstruction_mse": total_loss/count,
+        "originals": originals,
+        "reconstructions": reconstructions
+    }
+
+
+
+def visualize_reconstruction(
+    originals,
+    reconstructions,
+):
+
+    comparison = torch.cat(
+        [
+            originals,
+            reconstructions
+        ],
+        dim=0
+    )
+
+
+    grid = vutils.make_grid(
+        comparison,
+        nrow=len(originals),
+        normalize=True,
+        value_range=(-1,1)
+    )
+
+
+    plt.figure(
+        figsize=(12,4)
+    )
+
+    plt.imshow(
+        grid.permute(1,2,0)
+    )
+
+    plt.axis("off")
+    plt.show()
